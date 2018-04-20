0246f4eeae0675a5de15161692b115a8b57117af
Shadow992 <m.rupprecht@gmx.net>
2018-04-20 19:43:08 +0200

    Update theoryGraph.js

--- js/theoryGraph.js
+++ js/theoryGraph.js
@@ -1118,8 +1118,10 @@ function TheoryGraph()
 	function startRendering(fixedPositions=false) 
 	{
 		setStatusText("Rendering graph...");
+		console.log("fixedPositions: "+fixedPositions);
 		if(fixedPositions==false)
 		{
+			
 			if(typeof THEORY_GRAPH_OPTIONS.layout === 'undefined' || typeof THEORY_GRAPH_OPTIONS.layout.ownLayoutIdx === 'undefined' || THEORY_GRAPH_OPTIONS.layout.ownLayoutIdx==1)
 			{
 				var opti=new Optimizer(originalNodes,originalEdges);
