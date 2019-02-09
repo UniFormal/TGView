6e41ee5fb22d0319416cf3f7c1860a0875ff8b5d
Tom Wiesing <tkw01536@gmail.com>
2019-02-09 23:58:07 +0100

    Tweak webpack bundle config

--- src/demo/index.js
+++ src/demo/index.js
@@ -4,27 +4,29 @@
  * The main entry point for the TGView demo
  */
 
-import TGView from '..';
-
-// Load all of the required CSS
-import '../css/styles.css';
-// <link href="deps/fontawesome/css/font-awesome.min.css" rel="stylesheet" />
-import 'vis/dist/vis.min.css';
-//  <link href="deps/jqueryui/jquery-ui.min.css" rel="stylesheet" type="text/css">
-import 'jstree/dist/themes/default/style.min.css';
-
 // create an element and append it to the body
 const element = document.createElement('div');
+element.innerHTML = 'Loading TGView...';
 element.setAttribute('id', 'tgViewMainEle');
 element.setAttribute('class', 'ui-form');
 element.setAttribute('style', 'cursor: auto; overflow:hidden;');
 document.body.append(element);
 
-// once the window has loaded
-window.onload = function() {
-    const instance = new TGView({
-        prefix: 'test',
-        mainContainer: 'tgViewMainEle',
-        serverBaseURL: 'https://mmt.mathhub.info/'
-    });
-}
+Promise.all([
+    import('../'),
+    // Load all of the required CSS
+    import('../css/styles.css'),
+    // <link href="deps/fontawesome/css/font-awesome.min.css" rel="stylesheet" />
+    import('vis/dist/vis.min.css'),
+    //  <link href="deps/jqueryui/jquery-ui.min.css" rel="stylesheet" type="text/css">
+    import('jstree/dist/themes/default/style.min.css'),
+]).then(([TGView]) => {
+    // once the window has loaded
+    window.onload = function() {
+        const instance = new TGView({
+            prefix: 'test',
+            mainContainer: 'tgViewMainEle',
+            serverBaseURL: 'https://mmt.mathhub.info/'
+        });
+    }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -6,46 +6,59 @@
 const path = require("path");
 const HTMLPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  entry: "./src/demo",
-  mode: "development",
-  devtool: "inline-source-map",
-  module: {
-    rules: [
-      // source maps from typescript work
-      { test: /\.js$/, loader: "source-map-loader", enforce: "pre" },
-
-      // load typescript outside of node_modules
-      {
-        test: /\.ts$/,
-        use: "ts-loader",
-        exclude: /node_modules/
-      },
-      // load stylesheets -- only needed for development
-      {
-        test: /\.css$/,
-        loader: ["style-loader", "css-loader"]
-      },
-
-      // load png images -- only needed for development
-      {
-        test: /\.(png|jpg|gif)$/,
-        loader: "url-loader"
+module.exports = (_, {mode}) => {
+  mode = mode || "development";
+  
+  return {
+    entry: "./src/demo",
+    mode: mode,
+    devtool: mode === "development" ? "inline-source-map" : undefined,
+    module: {
+      rules: [
+        // source maps from typescript work
+        { test: /\.js$/, loader: "source-map-loader", enforce: "pre" },
+  
+        // load typescript outside of node_modules
+        {
+          test: /\.ts$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'ts-loader',
+              options: {
+                compilerOptions: {
+                  declaration: false,
+                }
+              }
+            }
+          ]
+        },
+        // load stylesheets -- only needed for development
+        {
+          test: /\.css$/,
+          loader: ["style-loader", "css-loader"]
+        },
+  
+        // load png images -- only needed for development
+        {
+          test: /\.(png|jpg|gif)$/,
+          loader: "file-loader"
+        }
+      ]
+    },
+    resolve: {
+      extensions: [".ts", ".js"]
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, "dist"),
+      devtoolModuleFilenameTemplate: mode === "development" ? "[absolute-resource-path]" : undefined,
+    },
+    optimization: {
+      splitChunks: {
+        chunks: "all"
       }
-    ]
-  },
-  resolve: {
-    extensions: [".ts", ".js"]
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, "dist"),
-    devtoolModuleFilenameTemplate: "[absolute-resource-path]"
-  },
-  optimization: {
-    splitChunks: {
-      chunks: "all"
-    }
-  },
-  plugins: [new HTMLPlugin()]
+    },
+    plugins: [new HTMLPlugin()]
+  }
 };
